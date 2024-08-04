00a73f5b9d351f205c9f48f66d8e53f152e07170
Jacob Millward <jacob@millward.dev>
2024-08-04 12:58:04 +0100

    Add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import shikiConfig from './shiki.config';
-
 import sitemap from '@astrojs/sitemap';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://jacob.millward.dev',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: shikiConfig.theme,
-		}
-	}
+  site: 'https://jacob.millward.dev',
+  integrations: [mdx(), sitemap(), preact()],
+  markdown: {
+    shikiConfig: {
+      theme: shikiConfig.theme
+    }
+  }
 });
