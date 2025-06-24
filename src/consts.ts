c5ea3c883f66d91821fb618e3cb3fceb93db9074
Jacob Millward-Yuan <jacob@millward.dev>
2025-06-24 20:08:59 +0100

    Update name

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jacob Millward';
-export const SITE_DESCRIPTION = 'Personal internet resting place of Jacob Millward. Software Developer, technologist, keen drinker of tea.';
-export const SITE_AUTHOR = 'Jacob Millward';
+export const SITE_TITLE = 'Jacob Millward-Yuan';
+export const SITE_DESCRIPTION = 'Personal internet resting place of Jacob Millward-Yuan. Software Developer, technologist, keen drinker of tea.';
+export const SITE_AUTHOR = 'Jacob Millward-Yuan';
